f8fb6080d0ac9080d67c1573c02ab0cd41163f2a
Matheus-Pontes <pontesm10@outlook.com>
2024-06-03 16:23:00 -0300

    Ajuste

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import { controleDeTeclaParaFormatacao } from "./util";
-
 const conteudoRft = document.getElementById("conteudo");
 const preview = document.getElementById("preview");
 const btnCopy = document.getElementById("copy");
 let selectedText;
+
+function controleDeTeclaParaFormatacao(event, tecla) {
+    if (event.ctrlKey && tecla === 'b') return "bold";
+
+    if (event.ctrlKey && tecla === 'i') return "italic";
+
+    if (event.ctrlKey && tecla === 'u') return "underline";
+
+    if (event.ctrlKey && tecla === 'x') return "subscript";
+
+    if (event.ctrlKey && tecla === 'w') return "superscript";
+
+    return "";
+}
 
 let actionsEditor = {
     conteudoFormatado: "",
